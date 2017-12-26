734147a77a7092a7da9ce659f39b9bfa7d8e0ab9
MAHAK RAJVANSHI <mahakrajvanshi@yahoo.com>
2017-12-26 15:08:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 function shout(string) {
 
-      return string.toUpperCase();
+      return string.toUpperCase()
 }
 
 function whisper(string){
 
-      return string.toLowerCase();
+      return string.toLowerCase()
+}
+
+function logShout(string){
+
+    console.log(string.toUpperCase())
 }
